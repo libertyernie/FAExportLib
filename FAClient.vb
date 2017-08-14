cac68a2a9d4a830054e1fd28b68ca0ec7b49ffcf
libertyernie <maybeway36@gmail.com>
2017-08-13 20:18:00 -0500

    Add FAExportException

--- FAClient.vb
+++ FAClient.vb
@@ -3,14 +3,22 @@ Imports System.Net
 Imports Newtonsoft.Json
 
 Public Class FAClient
-    Private Shared Async Function RequestAsync(url As String) As Task(Of String)
+    Public Sub New()
+
+    End Sub
+
+    Private Async Function RequestAsync(url As String) As Task(Of String)
         Dim request = WebRequest.CreateHttp(url)
-        request.UserAgent = "FAClient/0.1 (https://github.com/libertyernie/FAClient"
-        Using response = Await request.GetResponseAsync
-            Using sr As New StreamReader(response.GetResponseStream)
-                Return Await sr.ReadToEndAsync
+        request.UserAgent = "FAClient/0.1 (https://github.com/libertyernie/FAClient)"
+        Try
+            Using response = Await request.GetResponseAsync
+                Using sr As New StreamReader(response.GetResponseStream)
+                    Return Await sr.ReadToEndAsync
+                End Using
             End Using
-        End Using
+        Catch ex As WebException
+            Throw New FAExportException(ex)
+        End Try
     End Function
 
     Public Async Function GetUserAsync(name As String) As Task(Of FAUser)
---
+++ FAExportException.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+Imports System.Net
+Imports Newtonsoft.Json
+
+Public Class FAExportException
+    Inherits Exception
+
+    Public ReadOnly StatusCode As HttpStatusCode?
+    Public ReadOnly FAError As String
+    Public ReadOnly FAUrl As String
+
+    Public Sub New(ex As WebException)
+        MyBase.New(ex.Message)
+        StatusCode = TryCast(ex.Response, HttpWebResponse)?.StatusCode
+        Try
+            Using sr As New StreamReader(ex.Response.GetResponseStream)
+                Dim json = sr.ReadToEnd
+                Dim dict = JsonConvert.DeserializeObject(Of Dictionary(Of String, String))(json)
+                FAError = dict("error")
+                FAUrl = dict("url")
+            End Using
+            ex.Response.Close()
+        Catch ex2 As Exception
+
+        End Try
+    End Sub
+End Class
